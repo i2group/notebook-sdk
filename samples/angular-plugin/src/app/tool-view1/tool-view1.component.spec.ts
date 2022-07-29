5f16cfb9bb576eaa41c79b253a2479dda5a1a1f6
Jaravee Srilapan <Jaravee.Srilapan@i2group.com>
2022-07-29 10:03:11 +0100

    Publish SDK samples

---
+++ samples/angular-plugin/src/app/tool-view1/tool-view1.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ToolView1Component } from './tool-view1.component';
+
+describe('ToolView1Component', () => {
+  let component: ToolView1Component;
+  let fixture: ComponentFixture<ToolView1Component>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ToolView1Component ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ToolView1Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
